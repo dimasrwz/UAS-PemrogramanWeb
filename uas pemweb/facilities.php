d1e362c335d8234fe681d44f698784486679ab80
dimasr <89757844+dimasrwz@users.noreply.github.com>
2022-11-21 08:41:19 +0700

    UAS Pemrograman Web

---
+++ uas pemweb/facilities.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+    <title>Shikifujin Hotel - FACILITIES</title>
+    <?php require('inc/links.php')?>
+    <style>
+        .pop:hover{
+            border-top-color: var(--teal) !important;
+            transform: scale(1.03);
+            transition: all 0.3s;
+        }
+    </style>
+</head>
+
+<body class="bg-light">
+
+<?php
+require('inc/header.php');
+?>
+
+<div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Aut omnis quidem consectetur, eaque rem <br> dolores doloremque neque est sunt? 
+        Explicabo sit eius provident pariatur ratione. Maxime quasi reiciendis iure. Aspernatur!
+    </p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Maxime saepe a consequatur ipsa autem, eaque qui nobis ex? C
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Maxime saepe a consequatur ipsa autem, eaque qui nobis ex? C
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Maxime saepe a consequatur ipsa autem, eaque qui nobis ex? C
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Maxime saepe a consequatur ipsa autem, eaque qui nobis ex? C
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Maxime saepe a consequatur ipsa autem, eaque qui nobis ex? C
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Maxime saepe a consequatur ipsa autem, eaque qui nobis ex? C
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+require('inc/footer.php');
+?>
+
+</body>
+</html> 
